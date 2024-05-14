e34aced194f23016ab7814139a274db835840a06
qkrtpcks <qkrtpcks@naver.com>
2024-05-15 01:24:40 +0900

    ONION-57 Tof, yaw convert #done

--- Observer.c
+++ Observer.c
@@ -4,40 +4,6 @@
 #include <Observer.h>
 
 
-LuenbergerObserver observer;
-
-void Tof_to_yaw_Init(void){
-    initializeObserver(&observer);
-}
-
-// 관측기 초기화 함수
-void initializeObserver(LuenbergerObserver* observer) {
-    observer->estimated.yaw = 0.0;  // 초기 yaw 각도
-    observer->gain = OBSERVER_GAIN; // 관측기 이득
-}
-
-// 관측기 업데이트 함수
-void updateObserver(LuenbergerObserver* observer, int tof1, int tof2) {
-
-
-    double distance_difference = (double)(tof1 - tof2);
-    double scaled_distance_diff = TOF_DISTDIFF * (distance_difference / (fabs(distance_difference) + TOF_DISTDIFF));
-
-    // arcsin(y/x) 계산하여 라디안으로 반환
-    double yaw_angle_radians = asin(scaled_distance_diff / TOF_DISTDIFF);
-    double yaw_error = yaw_angle_radians - observer->estimated.yaw;
-    observer->estimated.yaw += observer->gain * yaw_error;
-}
-
-// 관측기에서 yaw 값을 반환하는 함수
-double getYawFromObserver(const LuenbergerObserver* observer) {
-    return observer->estimated.yaw;
-}
-
-
-///////
-
-
 double calculateYawAngle(int tof1, int tof2) {
 
     double distance_difference = (double)(tof1 - tof2);
@@ -45,6 +11,8 @@ double calculateYawAngle(int tof1, int tof2) {
 
     // arcsin(y/x) 계산하여 라디안으로 반환
     double yaw_angle_radians = asin(scaled_distance_diff / TOF_DISTDIFF);
+    yaw_angle_radians = yaw_angle_radians /PI * 180.0;
     return yaw_angle_radians;
 }
+
 
--- Observer.h
+++ Observer.h
@@ -1,29 +1,10 @@
 #ifndef OBSERVER_H_
 #define OBSERVER_H_
 
-#define OBSERVER_GAIN       0.1 // 크면 잡음에 민감?
 #define PI                  3.14159265358979323846
-#define TOF_DISTDIFF        52 //mm
-
-// 상태 변수 구조체
-typedef struct {
-    double yaw;       // 차량의 요각 추정값
-} State;
-
-// 루엔버거 관측기 상태 구조체
-typedef struct {
-    State estimated;  // 추정된 상태 변수
-    double gain;      // 관측기 이득
-} LuenbergerObserver;
-
-extern LuenbergerObserver observer;
-
-
-void Tof_to_yaw_Init(void);
-void initializeObserver(LuenbergerObserver* observer);
-void updateObserver(LuenbergerObserver* observer, int tof1, int tof2);
-double getYawFromObserver(const LuenbergerObserver* observer);
+#define TOF_DISTDIFF        78 //mm // 78
 
 double calculateYawAngle(int tof1, int tof2);
+
 
 #endif /* OBSERVER_H_ */
